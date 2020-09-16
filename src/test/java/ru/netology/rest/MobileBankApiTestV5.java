3ec128a3ccc7ea69c231261f6fbce843b1c91b5a
Dmitry Evdokimov <vlg1evdokimov@gmail.com>
2020-09-16 21:05:30 +0300

    Changed MobileBankApiTestV1-V6

--- src/test/java/ru/netology/rest/MobileBankApiTestV5.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -24,6 +24,7 @@ class MobileBankApiTestV5 {
           .statusCode(200)
           .contentType(ContentType.JSON)
           .body("every{ it.balance >= 0 }", is(true))
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
     }
 }
