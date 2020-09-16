1dd4f8b3a6b94caf46aa2e061592e808fcebc532
Dmitry Evdokimov <vlg1evdokimov@gmail.com>
2020-09-16 20:59:27 +0300

    Changed MobileBankApiTestV1

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -19,7 +19,7 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
-          .UnixCrypt.body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
